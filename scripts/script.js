ee8aeaa0f9abec9b71c4bff2018c1fbb751acbaa
Lawson Hung <lawsonhung@yahoo.com>
2025-12-22 19:14:57 -0500

    Add weather, cost. Get name description location inputs

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const newActivityForm = document.getElementById("newActivityForm");
+const nameInput = document.getElementById("nameInput");
+const descriptionInput = document.querySelector("#descriptionInput");
+const locationInput = document.querySelector("#locationInput");
 
 newActivityForm.addEventListener("submit", handleNewActivityFormSubmit);
 
@@ -6,4 +9,9 @@ function handleNewActivityFormSubmit(e) {
   e.preventDefault();
 
   console.log(e.target);
+  const name = nameInput.value;
+  const description = descriptionInput.value;
+  const location = locationInput.value;
+
+  console.log(name, description, location);
 }
