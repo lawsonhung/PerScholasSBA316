707a1f779e9793134fdf5ed0b0654a506391981a
Lawson Hung <lawsonhung@yahoo.com>
2025-12-22 19:48:05 -0500

    Append new activity to ul on form submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 const newActivityForm = document.getElementById("newActivityForm");
 const nameInput = document.getElementById("nameInput");
 const descriptionInput = document.querySelector("#descriptionInput");
-const locationInput = document.querySelector("#locationInput");
+const locationSelect = document.querySelector("#locationSelect");
+const weatherSelect = document.querySelector("#weatherSelect");
+const costInput = document.querySelector("#costInput");
+
+const adventureList = document.querySelector("#adventureList");
 
 newActivityForm.addEventListener("submit", handleNewActivityFormSubmit);
 
@@ -14,4 +18,32 @@ function handleNewActivityFormSubmit(e) {
   const location = locationInput.value;
 
   console.log(name, description, location);
+
+  addAdventure();
+}
+
+function addAdventure() {
+  const adventureToAdd = document.createDocumentFragment();
+  const liToAdd = document.createElement("li");
+
+  const name = nameInput.value;
+  const description = descriptionInput.value;
+  const location = locationSelect.value;
+  const weather = weatherSelect.value;
+  const cost = costInput.value;
+
+  const nameEl = document.createElement("p");
+  const summaryEl = document.createElement("p");
+  const descriptionEl = document.createElement("p");
+
+  nameEl.textContent = name;
+  summaryEl.textContent = `Going to ${location} on a ${weather} day will cost us $${cost}`;
+  descriptionEl.textContent = description;
+
+  liToAdd.appendChild(nameEl);
+  liToAdd.appendChild(summaryEl);
+  liToAdd.appendChild(descriptionEl);
+
+  adventureToAdd.appendChild(liToAdd);
+  adventureList.appendChild(adventureToAdd);
 }
